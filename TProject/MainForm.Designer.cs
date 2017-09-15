75afc67a3ec8ba828d08be8605e5eda026e7f86f
oleg.b.sam <oleg.b.sam@live.com>
2017-09-15 22:53:37 +0400

    перемещения и форма

--- TProject/MainForm.Designer.cs
+++ TProject/MainForm.Designer.cs
@@ -28,20 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabPageMap = new System.Windows.Forms.TabPage();
+            this.panelRightMenu = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panelMapSubstrate = new System.Windows.Forms.Panel();
+            this.pictureBoxMap = new System.Windows.Forms.PictureBox();
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.menuStrip.SuspendLayout();
+            this.tabPageMap.SuspendLayout();
+            this.panelRightMenu.SuspendLayout();
+            this.panelMapSubstrate.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMap)).BeginInit();
+            this.tabControlMain.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(775, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // tabPageMap
+            // 
+            this.tabPageMap.Controls.Add(this.panelRightMenu);
+            this.tabPageMap.Controls.Add(this.panelMapSubstrate);
+            this.tabPageMap.Location = new System.Drawing.Point(4, 22);
+            this.tabPageMap.Name = "tabPageMap";
+            this.tabPageMap.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageMap.Size = new System.Drawing.Size(767, 319);
+            this.tabPageMap.TabIndex = 0;
+            this.tabPageMap.Text = "Карта";
+            this.tabPageMap.UseVisualStyleBackColor = true;
+            // 
+            // panelRightMenu
+            // 
+            this.panelRightMenu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelRightMenu.Controls.Add(this.button1);
+            this.panelRightMenu.Controls.Add(this.checkBox1);
+            this.panelRightMenu.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRightMenu.Location = new System.Drawing.Point(583, 3);
+            this.panelRightMenu.Name = "panelRightMenu";
+            this.panelRightMenu.Size = new System.Drawing.Size(181, 313);
+            this.panelRightMenu.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(24, 33);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panelMapSubstrate
+            // 
+            this.panelMapSubstrate.AllowDrop = true;
+            this.panelMapSubstrate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelMapSubstrate.AutoScroll = true;
+            this.panelMapSubstrate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelMapSubstrate.Controls.Add(this.pictureBoxMap);
+            this.panelMapSubstrate.Location = new System.Drawing.Point(3, 3);
+            this.panelMapSubstrate.Name = "panelMapSubstrate";
+            this.panelMapSubstrate.Size = new System.Drawing.Size(574, 310);
+            this.panelMapSubstrate.TabIndex = 0;
+            // 
+            // pictureBoxMap
+            // 
+            this.pictureBoxMap.BackColor = System.Drawing.Color.White;
+            this.pictureBoxMap.Location = new System.Drawing.Point(48, 3);
+            this.pictureBoxMap.Name = "pictureBoxMap";
+            this.pictureBoxMap.Size = new System.Drawing.Size(410, 275);
+            this.pictureBoxMap.TabIndex = 0;
+            this.pictureBoxMap.TabStop = false;
+            this.pictureBoxMap.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxMap_Paint);
+            this.pictureBoxMap.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxMap_MouseDown);
+            this.pictureBoxMap.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxMap_MouseMove);
+            this.pictureBoxMap.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxMap_MouseUp);
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabPageMap);
+            this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlMain.Location = new System.Drawing.Point(0, 24);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(775, 345);
+            this.tabControlMain.TabIndex = 2;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(775, 369);
+            this.Controls.Add(this.tabControlMain);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
             this.Text = "3GIS";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.tabPageMap.ResumeLayout(false);
+            this.panelRightMenu.ResumeLayout(false);
+            this.panelRightMenu.PerformLayout();
+            this.panelMapSubstrate.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMap)).EndInit();
+            this.tabControlMain.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.TabPage tabPageMap;
+        private System.Windows.Forms.Panel panelMapSubstrate;
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.Panel panelRightMenu;
+        private System.Windows.Forms.PictureBox pictureBoxMap;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- TProject/MainForm.cs
+++ TProject/MainForm.cs
@@ -15,6 +15,49 @@ namespace TProject
         public MainForm()
         {
             InitializeComponent();
+            mas.Add(rect = new Rectangle(20, 20, 50, 50));
+            mas.Add(new Rectangle(40, 20, 50, 50));
+        }
+        Rectangle rect;
+        List<Rectangle> mas = new List<System.Drawing.Rectangle>();
+        private void pictureBoxMap_Paint(object sender, PaintEventArgs e)
+        {
+
+            Pen pen = new Pen(Color.Black);
+            e.Graphics.DrawEllipse(pen, re);
+            foreach (var r in mas)
+                e.Graphics.DrawEllipse(pen, r);
+        }
+
+        int dX, dY;
+        bool isClicked = false;
+
+        private void pictureBoxMap_MouseUp(object sender, MouseEventArgs e)
+        {
+            isClicked = false;
+            mas.Add(re);
+        }
+
+        private void pictureBoxMap_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isClicked)
+            {
+                re.X = e.X - dX;
+                re.Y = e.Y - dY;
+                pictureBoxMap.Invalidate();
+            }
+        }
+        Rectangle re;
+        private void pictureBoxMap_MouseDown(object sender, MouseEventArgs e)
+        {
+            re = mas.Find(o => e.X < o.X + o.Width && e.X > o.X && e.Y < o.Y + o.Height && e.Y > o.Y);
+            if (re  != null)
+            {
+                mas.Remove(re);
+                isClicked = true;
+                dX = e.X - re.X;
+                dY = e.Y - re.Y;
+            }
         }
     }
 }
