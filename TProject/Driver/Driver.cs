c0ad2d86e0756138f3e7c40c0d4aec1ec5d6b2e2
oleg.b.sam <oleg.b.sam@live.com>
2017-09-20 21:04:26 +0400

    123

--- TProject/Driver/Driver.cs
+++ TProject/Driver/Driver.cs
@@ -5,6 +5,9 @@
         public Car Car { get; set; }
         public string DriverName { get; set; }
 
+        public int OverSpeed { get; set; }
+        public bool isViolateTL { get; set; }
+
         public Driver(string typeName, string driverName,Car car):base(typeName)
         {
             Car = car;
