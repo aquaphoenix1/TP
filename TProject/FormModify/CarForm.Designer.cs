3f6e30557f6172d0841ef522bbc58dc274198672
asd <asd@DSA>
2017-11-27 21:23:36 +0300

    База данных

--- TProject/FormModify/CarForm.Designer.cs
+++ TProject/FormModify/CarForm.Designer.cs
@@ -42,7 +42,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Location = new System.Drawing.Point(12, 53);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 16);
             this.label1.TabIndex = 0;
