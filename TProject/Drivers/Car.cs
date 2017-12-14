39c0ed1630e04c757c7653360c767b1c156d0c8a
Лякишев Антон <aqua_phoenix@mail.ru>
2017-12-14 15:36:56 +0400

    й

--- TProject/Drivers/Car.cs
+++ TProject/Drivers/Car.cs
@@ -4,11 +4,24 @@ namespace TProject.Driver
 {
     public class Car : Type
     {
+        /// <summary>
+        /// Список всех автомобилей
+        /// </summary>
         public static List<List<object>> ListAuto { get; set; }
+
         public Fuel CarFuel { get; set; }
+
         public double FuelConsumption { get; set; }
+
         public double Speed { get; set; }
 
+        /// <summary>
+        /// Создание автомобиля
+        /// </summary>
+        /// <param name="model">Модель</param>
+        /// <param name="fuel">Топливо</param>
+        /// <param name="consumption">Потребление топлива</param>
+        /// <param name="speed">Скорость автомобиля</param>
         public Car(string model, Fuel fuel, double consumption, double speed) : base(model)
         {
             this.CarFuel = fuel;
@@ -18,6 +31,14 @@ namespace TProject.Driver
 
         private Car() { }
 
+        /// <summary>
+        /// Создание автомобиля. Фабрика.
+        /// </summary>
+        /// <param name="model">Модель</param>
+        /// <param name="fuel">Топливо</param>
+        /// <param name="consumption">Потребление топлива</param>
+        /// <param name="speed">Скорость автомобиля</param>
+        /// <returns>Автомобиль</returns>
         public static Car CreateCar(string model, Fuel fuel, double consumption, double speed)
         {
             return new Car
--- TProject/Drivers/Fuel.cs
+++ TProject/Drivers/Fuel.cs
@@ -4,9 +4,18 @@ namespace TProject.Driver
 {
     public class Fuel : Type
     {
+        /// <summary>
+        /// Список топлива
+        /// </summary>
         public static List<List<object>> ListFuel { get; set; }
+
         public double Price { get; set; }
 
+        /// <summary>
+        /// СОздание топлива
+        /// </summary>
+        /// <param name="typeName">Название топлива</param>
+        /// <param name="price">Цена</param>
         public Fuel(string typeName, double price) : base(typeName)
         {
             this.Price = price;
